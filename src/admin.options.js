5465682ab31a787bc5364d2d4085198765dcc1db
Roberto Nobre <robertonobre90@gmail.com>
2020-06-18 22:59:33 -0300

    translate and user entity

--- src/admin.options.js
+++ src/admin.options.js
@@ -4,10 +4,19 @@ const AdminBroMongoose = require('admin-bro-mongoose');
 AdminBro.registerAdapter(AdminBroMongoose);
 
 const { Company } = require('./companies/company.entity');
+const { User } = require('./user/user.entity');
 
 /** @type {import('admin-bro').AdminBroOptions} */
 const options = {
-  resources: [Company],
+  resources: [Company, User],
+  locale: {
+    language: 'pt',
+    translations: {
+      actions: {
+        new: 'Novo',
+      },
+    },
+  },
 };
 
 module.exports = options;
