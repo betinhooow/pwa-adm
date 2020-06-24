7f5fdfb9815438f6eea801a44a5bd25073ba2fc6
Roberto Nobre <robertonobre90@gmail.com>
2020-06-24 19:44:59 -0300

    Footer component

---
+++ src/layout/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="pull-right">With some coffee</div>
+    <div>Developed By @betinhooow</div>
+  </div>
+)
+
+export default Footer;
