c08934b7879c87dac0485e759e3ad405720faefd
Roberto Nobre <robertonobre90@gmail.com>
2020-07-24 23:08:22 -0300

    Home and modal routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,11 +4,15 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import Table from '../pages/Table/Table';
 import SignIn from '../pages/SignIn/SignIn';
+import Home from '../pages/Home/Home';
+import Modal from '../pages/Modal/Modal';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/table" component={Table} isPrivate />
+    <Route path="/home" component={Home} isPrivate />
+    <Route path="/modal" component={Modal} isPrivate />
   </Switch>
 );
 
