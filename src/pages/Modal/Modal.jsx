8b8d999052bb4cdb6176bb52d94a2a560ce1a30e
Roberto Nobre <robertonobre90@gmail.com>
2020-07-25 20:36:04 -0300

    Modal page

--- src/pages/Modal/Modal.jsx
+++ src/pages/Modal/Modal.jsx
@@ -18,8 +18,33 @@ const ModalPage = () => {
       >
         Large Modal
       </button>
-      
-      <Modal open={open} onClose={() => setOpen(false)} />
+
+      <Modal open={open} onClose={() => setOpen(false)} title="Modal title">
+        <div className="modal-body">
+          <p>
+            <strong>Lorem Ipsum is simply dummy</strong> text of the printing
+            and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged.
+          </p>
+          <p>
+            <strong>Lorem Ipsum is simply dummy</strong> text of the printing
+            and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged.
+          </p>
+        </div>
+
+        <div className="modal-footer">
+          <button type="button" className="btn btn-primary">
+            Save changes
+          </button>
+        </div>
+      </Modal>
       <Footer />
     </div>
   );
