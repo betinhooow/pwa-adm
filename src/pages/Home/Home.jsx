54c75eee3a3e207fad8e3c91db80e05cd8572016
Roberto Nobre <robertonobre90@gmail.com>
2020-07-24 22:27:40 -0300

    Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../layout/Header";
+import Footer from "../../layout/Footer";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
