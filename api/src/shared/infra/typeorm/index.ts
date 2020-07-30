af4abfbf967ce8ed330355a7b6f712dfcb70e43f
Roberto Nobre <robertonobre90@gmail.com>
2020-07-29 22:41:19 -0300

    connection config on typeorm initializer

--- api/src/shared/infra/typeorm/index.ts
+++ api/src/shared/infra/typeorm/index.ts
@@ -1,3 +1,16 @@
-import { createConnections } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-createConnections();
+createConnection({
+  "name": "default",
+  "type": "mysql",
+  "host": "localhost",
+  "port": 3306,
+  "username": "username",
+  "password": "password",
+  "database": "baasename",
+  "entities": [__dirname + "./../../../../dist/modules/users/infra/typeorm/entities/User.js"],
+  "migrations": [__dirname + "./../../../../dist/shared/infra/typeorm/migrations/*.js"],
+  "cli": {
+    "migrationsDir": __dirname + "./../../../../dist/shared/infra/typeorm/migrations"
+  }
+});
